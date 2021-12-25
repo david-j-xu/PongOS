9a9d983d1b0a7550687dab03e5a20981f88e1280
david-j-xu <63666552+davidjxu21@users.noreply.github.com>
2021-12-25 14:39:58 -0500

    loaded additional sectors

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -2,15 +2,47 @@
 org 0x7c00 ; preconfigure offset of boot block
 
 write:
-    mov si, boot_message ; load string into source register
+    mov bx, BOOT_MESSAGE ; load string into source register
     call print_string   ; print the string
-    jmp $               ; loop ad infinitum
+
+set_boot_drive:
+    mov [BOOT_DRIVE], dl
+
+set_stack:
+    mov bp, 0x8000          ; set stack base pointer to 0x8000
+    mov sp, bp              ; set stack pointer to base pointer
+
+load_next_sectors:
+    mov bx, 0x0000              ; set ES to 0x0000
+    mov es, bx                  ; set ES to 0x0000
+    mov bx, 0x9000              ; Load next sectors to ES:0x9000
+    mov dh, [LOAD_NUM_SECTORS]  ; Specify the number of sectors to be loaded
+    mov dl, [BOOT_DRIVE]        ; load from the boot drive
+    call load_sectors           ; load the drives
+
+test_load:
+    mov ax, [0x9000]
+    call print_char
+    mov ax, [0x9000 + 512]
+    call print_char
+
+    jmp $
 
 %include "src/boot/utils.asm"
 
-boot_message:
-    db 'Booting OS', 0  ; null terminated string
+BOOT_DRIVE:
+    db 0                ; store boot drive number here
+BOOT_MESSAGE:
+    db `Booting PongOS...\n\r`, 0  ; null terminated string
+LOAD_NUM_SECTORS:
+    db 3
 
 ; Marking as bootable sector
 times 510-($-$$) db 0  ; padding
 dw 0xaa55 ; magic number
+
+; second sector code:
+times 256 dw 0x6162
+times 256 dw 0x6364
+times 256 dw 0x6566
+times 256 dw 0x6768
--- src/boot/utils.asm
+++ src/boot/utils.asm
@@ -2,10 +2,11 @@
 
 print_string:
     .loop:
-        lodsb           ; loads next byte into al
+        mov al, [bx]    ; moves character at bx to al
         cmp al, 0x00    ; checks if null
         je .done        ; if so, end loop
         call print_char ; print character
+        add bx, 0x01    ; increment address
         jmp .loop       ; loop
     .done:
         ret             ; return to caller
@@ -15,3 +16,26 @@ print_char:
     mov ah, 0x0e    ; set AH to 0eh to print character
     int 0x10        ; interrupt to print character
     ret
+
+load_sectors:
+    ; loads dh sectors after the boot sector from drive dl
+    push dx         ; save argument for error checking
+    mov ah, 0x02    ; set up for read
+    mov al, dh      ; read dh sectors
+    mov ch, 0x00    ; cylinder 0
+    mov dh, 0x00    ; head 0
+    mov cl, 0x02    ; sector 0
+    int 0x13        ; interrupt
+    jc disk_error   ; if carry, halt
+    pop dx          ; check correct number of sectors read
+    cmp dh, al      ; check dh == number of sectors read
+    jne disk_error  ; if so, print an error
+    ret
+
+disk_error:
+    mov bx, DISK_ERROR_MSG 
+    call print_string
+    jmp $
+
+DISK_ERROR_MSG:
+    db `Disk read error!\n\r`, 0
